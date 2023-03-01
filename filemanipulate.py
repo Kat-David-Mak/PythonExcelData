16ba4319e1a53299d4a8d5eb98b1286b2a44d3c4
Katleho Makoetje <katlehomakoetje@gmail.com>
2023-03-01 13:21:29 +0200

    Added Removed r in read_file

--- filemanipulate.py
+++ filemanipulate.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-read_file = pd.read_excel(r'Basotho_food_Class.xlsx',sheet_name='Sheet1')
+read_file = pd.read_excel('Basotho_food_Class.xlsx',sheet_name='Sheet1')
 read_file.to_csv('Basotho_Food_CSV.csv', index=None, header=True)
 basotho_data = pd.read_csv('Basotho_Food_CSV.csv')
 #display the first five rows 
